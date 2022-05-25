b0dae3cacb56ffd948ef2da8cc74f7d467767631
Michel Lamarlière <m.lamarliere@icloud.com>
2022-05-25 20:07:45 +0200

    Landing page only repo.

--- server/routes/contact-routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const contactControllers = require('../controllers/contact-controllers');
-
-const router = Router();
-
-router.post('/message', contactControllers.sendMessage);
-
-module.exports = router;
